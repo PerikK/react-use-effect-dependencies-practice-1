6f1aee7ccfd98d16e5a4e2c52bbfb25b1560e852
Periklis <perik76@gmail.com>
2024-05-01 13:22:18 +0300

    Done but conitional render needs work

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,40 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import DataList from "./components/DataList.jsx";
 import SelectTypeForm from "./components/SelectTypeForm.jsx";
 import "./App.css";
 
 export default function App() {
   const [dataType, setDataType] = useState("");
-
   const [data, setData] = useState(null);
-
+  
   console.log({ data });
+  console.log({ dataType });
 
   // Write code here.
   //
+
+  useEffect(() => {
+		if (dataType === "people") {
+			fetch("https://swapi.dev/api/people/")
+				.then((response) => response.json())
+				.then(setData)
+			return
+		}
+		if (dataType === "planets") {
+			fetch("https://swapi.dev/api/planets/")
+				.then((response) => response.json())
+				.then(setData)
+			return
+		}
+		if (dataType === "starships") {
+			fetch("https://swapi.dev/api/starships/")
+				.then((response) => response.json())
+				.then(setData)
+			return
+		}
+	}, [dataType])
+
+
 
   return (
     <div>
